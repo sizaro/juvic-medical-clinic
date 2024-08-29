392287e035b589eb3d9c714d20e27872258f771a
sizaro <spartansiza@gmail.com>
2024-08-29 10:01:37 -0400

    x humburger

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,11 @@
 const lineHumburger = document.querySelector(".line-humburger") 
 const navigation = document.querySelector(".navigation") 
+const xHumburger = document.querySelector(".x-humburger") 
 
 lineHumburger.addEventListener("click", ()=>{
-    navigation.classList.toggle("active")
+    navigation.classList.add("active")
+})
+
+xHumburger.addEventListener("click", ()=>{
+    navigation.classList.remove("active")
 })
